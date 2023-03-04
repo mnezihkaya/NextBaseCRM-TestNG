191b4d7d2342d7db39a2b159fd7a843b1214bcf5
nezih <m.nezihkaya@gmail.com>
2023-03-04 18:42:16 -0500

    ConfigurationReader utility added

--- src/test/java/com/Utilities/ConfigurationReader.java
+++ src/test/java/com/Utilities/ConfigurationReader.java
@@ -5,32 +5,31 @@ import java.io.IOException;
 import java.util.Properties;
 
 public class ConfigurationReader {
-        //1- Create the object of Properties
-        private static Properties properties = new Properties();
+    //1- Create the object of Properties
+    private static Properties properties = new Properties();
 
-        static {
+    static {
 
-            try {
+        try {
 
-                //2- We need to open the file in java memory: FileInputStream
-                FileInputStream file = new FileInputStream("configuration.properties");
+            //2- We need to open the file in java memory: FileInputStream
+            FileInputStream file = new FileInputStream("configuration.properties");
 
-                //3- Load the properties object using FileInputStream object
-                properties.load(file);
+            //3- Load the properties object using FileInputStream object
+            properties.load(file);
 
-                //close the file
-                file.close();
+            //close the file
+            file.close();
 
 
-            } catch (IOException e) {
-                System.out.println("File not found in the ConfigurationReader class.");
-                e.printStackTrace();
-            }
-
+        } catch (IOException e) {
+            System.out.println("File not found in the ConfigurationReader class.");
+            e.printStackTrace();
         }
 
-        public static String getProperty(String keyword) {
-            return properties.getProperty(keyword);
-        }
+    }
+
+    public static String getProperty(String keyword) {
+        return properties.getProperty(keyword);
     }
 }
