80ee83e16adfc8b8df51358c39a1bcf5e75d3903
nezih <m.nezihkaya@gmail.com>
2023-03-05 23:40:12 -0500

    ProfileTabTest test finished

--- src/test/java/com/StepDefinitions/ProfileTabTest.java
+++ src/test/java/com/StepDefinitions/ProfileTabTest.java
@@ -1,6 +1,11 @@
 package com.StepDefinitions;
 
+import com.Page.LoginPage;
 import com.Page.MainPage;
+import com.Utilities.BrowserUtils;
+import com.Utilities.CRMUtilities;
+import com.Utilities.ConfigurationReader;
+import com.Utilities.Driver;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -12,22 +17,30 @@ import java.util.List;
 public class ProfileTabTest extends DataProviderForTest{
 
     //check my profile
-    @Test(dataProvider = "userTypes",groups = {"P1"})
+    @Test(dataProvider = "userTypes")
     public static void ProfileTabs(String userName) {
 
         MainPage mainPage=new MainPage();
+        LoginPage loginPage=new LoginPage();
 
-        List<String>expectedProfileTabs=new ArrayList<>(Arrays.asList("My Profile","Edit Profile Settings","Themes","Configure notifications","Log Out"));
+        //login as
+        CRMUtilities.LogIn(userName);
+
+        BrowserUtils.highlight(mainPage.profile);
+
+        mainPage.profile.click();
+        BrowserUtils.sleep(2);
+
+        List<String>expectedProfileTabs=new ArrayList<>(Arrays.asList("My Profile","Edit Profile Settings","Themes","Configure notifications","Log out"));
+
         List<String>actualProfileTabs=new ArrayList<>();
         for (WebElement each: mainPage.profileTab_allModules) {
             actualProfileTabs.add(each.getText());
         }
         Assert.assertEquals(actualProfileTabs,expectedProfileTabs);
 
-        //My Profile
-        //Edit Profile Settings
-        //Themes
-        //Configure notifications
-        //Log Out
+        BrowserUtils.highlight(loginPage.logOutButton);
+        loginPage.logOutButton.click();
+
     }
 }
